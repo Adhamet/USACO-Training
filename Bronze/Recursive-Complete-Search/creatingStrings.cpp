c869c827764aeb89d7aa10a3eb8d52ae620a7cd8
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2024-02-28 10:15:52 +0200

    recursive method

--- Bronze/Recursive-Complete-Search/creatingStrings.cpp
+++ Bronze/Recursive-Complete-Search/creatingStrings.cpp
@@ -30,3 +30,42 @@ int main()
 
     return 0;
 }
+
+/**
+ * Recursive method
+ */
+/*
+string s;
+vector<string> perms;
+int char_count[26];
+
+void search(const string &curr = "") {
+    if(curr.size == s.size()) {
+        // permutation found
+        perms.push_back(curr);
+        return;
+    }
+
+    for(int i = 0; i < 26; i++) {
+        // Loop over all available characters
+        if(char_count[i] > 0) {
+            // Put it in its position in string
+            // Then continue searching.
+            char_count[i]--;
+            search(curr + (char)('a'+i));
+            char_count[i]++;
+        }
+    }
+}
+
+int main()
+{
+    cin >> s;
+    for(char c: s) char_count[c - 'a']++;
+
+    search();
+
+    cout << perms.size() << el;
+    for(const string &perm: perms) cout << perm << el;
+}
+*/
